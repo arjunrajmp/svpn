230efa0b5b4502193e05721820e8dc54a861179d
Arjun <arjunrajmp@gmail.com>
2021-12-12 14:22:07 +0530

    updated files

--- terms-and-conditions.php
+++ terms-and-conditions.php
@@ -1,1 +1,124 @@
-<div class="p-rich_text_block" dir="auto"><div class="p-rich_text_section"><b data-stringify-type="bold">Terms &amp; Conditions</b><br>By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages or make derivative versions. The app itself, and all the trademarks, copyright, database rights, and other intellectual property rights related to it, still belong to <i data-stringify-type="italic">Dr wireguard.</i><br><i data-stringify-type="italic">Dr wireguard</i> is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.<br>The Special app stores and processes personal data that you have provided to us, to provide my Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the Special app won’t work properly or at all.<br>The app does use third-party services that declare their Terms and Conditions.<br>Link to Terms and Conditions of third-party service providers used by the app<br></div><ul data-stringify-type="unordered-list" class="p-rich_text_list p-rich_text_list__bullet" data-indent="0" data-border="false" data-border-radius-top-cap="false" data-border-radius-bottom-cap="false"><li data-stringify-indent="0" data-stringify-border="0"><a target="_blank" class="c-link" data-stringify-link="https://policies.google.com/terms" delay="150" data-sk="tooltip_parent" href="https://policies.google.com/terms" rel="noopener noreferrer" tabindex="-1" data-remove-tab-index="true">Google Play Services</a></li><li data-stringify-indent="0" data-stringify-border="0"><a target="_blank" class="c-link" data-stringify-link="https://firebase.google.com/terms/analytics" delay="150" data-sk="tooltip_parent" href="https://firebase.google.com/terms/analytics" rel="noopener noreferrer" tabindex="-1" data-remove-tab-index="true">Google Analytics for Firebase</a></li><li data-stringify-indent="0" data-stringify-border="0"><a target="_blank" class="c-link" data-stringify-link="https://firebase.google.com/terms/crashlytics" delay="150" data-sk="tooltip_parent" href="https://firebase.google.com/terms/crashlytics" rel="noopener noreferrer" tabindex="-1" data-remove-tab-index="true">Firebase Crashlytics</a></li></ul><div class="p-rich_text_section">You should be aware that there are certain things that <i data-stringify-type="italic">Dr wireguard</i> will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi or provided by your mobile network provider, but <i data-stringify-type="italic">Dr wireguard</i> cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.<br>If you’re using the app outside of an area with Wi-Fi, you should remember that the terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app, or other third-party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app.<br>Along the same lines, <i data-stringify-type="italic">Dr wireguard</i> cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, <i data-stringify-type="italic">Dr wireguard</i> cannot accept responsibility.<br>With respect to <i data-stringify-type="italic">Dr wireguard</i>’s responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavour to ensure that it is updated and correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. <i data-stringify-type="italic">Dr wireguard</i> accepts no liability for any loss, direct or indirect, you experience as a result of relying wholly on this functionality of the app.<br>At some point, we may wish to update the app. The app is currently available on Android – the requirements for the system(and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. <i data-stringify-type="italic">Dr wireguard</i> does not promise that it will always update the app so that it is relevant to you and/or works with the Android version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device.<br><b data-stringify-type="bold">Changes to This Terms and Conditions</b><br>I may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Terms and Conditions on this page.<br>These terms and conditions are effective as of 2021-12-09<br><b data-stringify-type="bold">Contact Us</b><br>If you have any questions or suggestions about my Terms and Conditions, do not hesitate to contact me at <a target="_blank" class="c-link" data-stringify-link="mailto:dr.wireguard@gmail.com" delay="150" data-sk="tooltip_parent" aria-haspopup="menu" aria-expanded="false" href="mailto:dr.wireguard@gmail.com" rel="noopener noreferrer" tabindex="-1" data-remove-tab-index="true">dr.wireguard@gmail.com<span aria-label="(opens in new tab)"></span></a>.<br>This Terms and Conditions page was generated by&nbsp;<a target="_blank" class="c-link" data-stringify-link="https://app-privacy-policy-generator.nisrulz.com/" delay="150" data-sk="tooltip_parent" href="https://app-privacy-policy-generator.nisrulz.com/" rel="noopener noreferrer" tabindex="-1" data-remove-tab-index="true">App Privacy Policy Generator</a></div></div>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Terms &amp; Conditions</title>
+    </head>
+    <body>
+        <div class="p-rich_text_block" dir="auto" style="font-size:12px">
+            <div class="p-rich_text_section">
+                <b data-stringify-type="bold">Terms &amp; Conditions</b><br />
+                By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy or modify the app, any part of the
+                app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages or make derivative versions. The app itself, and
+                all the trademarks, copyright, database rights, and other intellectual property rights related to it, still belong to <i data-stringify-type="italic">App publisher</i><br />.
+                <i data-stringify-type="italic">App publisher</i> is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services,
+                at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.<br />
+                The Special app stores and processes personal data that you have provided to us, to provide my Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not
+                jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious
+                programs, compromise your phone’s security features and it could mean that the Special app won’t work properly or at all.<br />
+                The app does use third-party services that declare their Terms and Conditions.<br />
+                Link to Terms and Conditions of third-party service providers used by the app<br />
+            </div>
+            <ul data-stringify-type="unordered-list" class="p-rich_text_list p-rich_text_list__bullet" data-indent="0" data-border="false" data-border-radius-top-cap="false" data-border-radius-bottom-cap="false">
+                <li data-stringify-indent="0" data-stringify-border="0">
+                    <a
+                        target="_blank"
+                        class="c-link"
+                        data-stringify-link="https://policies.google.com/terms"
+                        delay="150"
+                        data-sk="tooltip_parent"
+                        href="https://policies.google.com/terms"
+                        rel="noopener noreferrer"
+                        tabindex="-1"
+                        data-remove-tab-index="true"
+                    >
+                        Google Play Services
+                    </a>
+                </li>
+                <li data-stringify-indent="0" data-stringify-border="0">
+                    <a
+                        target="_blank"
+                        class="c-link"
+                        data-stringify-link="https://firebase.google.com/terms/analytics"
+                        delay="150"
+                        data-sk="tooltip_parent"
+                        href="https://firebase.google.com/terms/analytics"
+                        rel="noopener noreferrer"
+                        tabindex="-1"
+                        data-remove-tab-index="true"
+                    >
+                        Google Analytics for Firebase
+                    </a>
+                </li>
+                <li data-stringify-indent="0" data-stringify-border="0">
+                    <a
+                        target="_blank"
+                        class="c-link"
+                        data-stringify-link="https://firebase.google.com/terms/crashlytics"
+                        delay="150"
+                        data-sk="tooltip_parent"
+                        href="https://firebase.google.com/terms/crashlytics"
+                        rel="noopener noreferrer"
+                        tabindex="-1"
+                        data-remove-tab-index="true"
+                    >
+                        Firebase Crashlytics
+                    </a>
+                </li>
+            </ul>
+            <div class="p-rich_text_section">
+                You should be aware that there are certain things that <i data-stringify-type="italic">App publisher/i> will not take responsibility for. Certain functions of the app will require the app to have an active internet
+                connection. The connection can be Wi-Fi or provided by your mobile network provider, but <i data-stringify-type="italic">Dr wireguard</i> cannot take responsibility for the app not working at full functionality if you don’t
+                have access to Wi-Fi, and you don’t have any of your data allowance left.<br />
+                If you’re using the app outside of an area with Wi-Fi, you should remember that the terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the
+                cost of data for the duration of the connection while accessing the app, or other third-party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app
+                outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received
+                permission from the bill payer for using the app.<br />
+                Along the same lines, <i data-stringify-type="italic">App publisher</i> cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and
+                you can’t turn it on to avail the Service, <i data-stringify-type="italic">App publisher</i> cannot accept responsibility.<br />
+                With respect to <i data-stringify-type="italic">App publisher</i>’s responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavour to ensure that it is updated and
+                correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. <i data-stringify-type="italic">App publisher</i> accepts no liability for any loss, direct or indirect,
+                you experience as a result of relying wholly on this functionality of the app.<br />
+                At some point, we may wish to update the app. The app is currently available on Android – the requirements for the system(and for any additional systems we decide to extend the availability of the app to) may change, and
+                you’ll need to download the updates if you want to keep using the app. <i data-stringify-type="italic">App publisher</i> does not promise that it will always update the app so that it is relevant to you and/or works with the
+                Android version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any
+                time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed)
+                delete it from your device.<br />
+                <b data-stringify-type="bold">Changes to This Terms and Conditions</b><br />
+                I may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Terms and Conditions on this page.<br />
+                These terms and conditions are effective as of 2021-12-09<br />
+                <b data-stringify-type="bold">Contact Us</b><br />
+                If you have any questions or suggestions about my Terms and Conditions, do not hesitate to contact me at
+                <a
+                    target="_blank"
+                    class="c-link"
+                    data-stringify-link="mailto:dr.wireguard@gmail.com"
+                    delay="150"
+                    data-sk="tooltip_parent"
+                    aria-haspopup="menu"
+                    aria-expanded="false"
+                    href="mailto:dr.wireguard@gmail.com"
+                    rel="noopener noreferrer"
+                    tabindex="-1"
+                    data-remove-tab-index="true"
+                >
+                    dr.wireguard@gmail.com<span aria-label="(opens in new tab)"></span>
+                </a>
+                .<br />
+                This Terms and Conditions page was generated by&nbsp;
+                <a
+                    target="_blank"
+                    class="c-link"
+                    data-stringify-link="https://app-privacy-policy-generator.nisrulz.com/"
+                    delay="150"
+                    data-sk="tooltip_parent"
+                    href="https://app-privacy-policy-generator.nisrulz.com/"
+                    rel="noopener noreferrer"
+                    tabindex="-1"
+                    data-remove-tab-index="true"
+                >
+                    App Privacy Policy Generator
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
